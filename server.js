d007e1bdb4cded95c588adae9e25b3459e478d9c
stamovm <m.stamov@yahoo.com>
2022-01-15 13:25:48 -0800

    added bootstrap created card printed card info for each card

--- server.js
+++ server.js
@@ -1,2 +1,25 @@
-console.log('server log 1')
-console.log('server log 2')
+const express = require('express')
+const articleRouter = require('./routes/articles')
+const app = express()
+const PORT = 5000
+
+app.use('/articles', articleRouter)
+app.set('view engine', 'ejs')
+app.get('/', (req, res) => {
+  const articles = [
+    {
+      title: 'Article test',
+      createdAt: new Date(),
+      description: 'Test description',
+    },
+    {
+      title: 'Article 2',
+      createdAt: new Date(),
+      description: 'description 2',
+    },
+  ]
+  res.render('articles/index', { articles: articles })
+})
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`)
+})
